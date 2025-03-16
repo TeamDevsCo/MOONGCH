7b0d3504f4ee859c97b7939a1a013664d7d28c4f
Jhung Minwoo <minwoorich96@gmail.com>
2025-03-16 11:31:45 +0900

    🚧 JPA 엔티티 작업중

---
+++ src/main/java/com/devsco/moongch/moongchs/infrastructure/PersonalMoongchEntity.java
@@ -0,0 +1,25 @@
+package com.devsco.moongch.moongchs.infrastructure;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(schema = "moongch", name = "personal_moongch")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@Builder
+public class PersonalMoongchEntity {
+
+  @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+}
