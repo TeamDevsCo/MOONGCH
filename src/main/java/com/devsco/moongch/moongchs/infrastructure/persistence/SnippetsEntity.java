82c556b6de0e26981a958babecb112e5208e2b8d
Jhung Minwoo <minwoorich96@gmail.com>
2025-03-16 18:12:15 +0900

    ✨ JPA 엔티티 생성

---
+++ src/main/java/com/devsco/moongch/moongchs/infrastructure/persistence/SnippetsEntity.java
@@ -0,0 +1,29 @@
+package com.devsco.moongch.moongchs.infrastructure.persistence;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(schema = "moongch", name = "snippets")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@Builder
+public class SnippetsEntity {
+  @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+  private Long moongchId;
+  private String title;
+  @Column(columnDefinition = "text")
+  private String contents;
+  private String ext;
+}
