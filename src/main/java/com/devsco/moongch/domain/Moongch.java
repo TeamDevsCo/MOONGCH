44c9a706d448ff401461d2abb8e4e386f9ea4644
Jhung Minwoo <minwoorich96@gmail.com>
2025-04-15 22:40:39 +0900

    🚧 뭉치 도메인 엔티티 설계중

---
+++ src/main/java/com/devsco/moongch/domain/Moongch.java
@@ -0,0 +1,10 @@
+package com.devsco.moongch.domain;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+public class Moongch {
+  private final Long moongchId;
+  private final String title;
+  private final String descrition;
+}
